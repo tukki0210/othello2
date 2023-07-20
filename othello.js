84b61e9effca6073a8e4a838e61f489da478ab3a
tukki0210 <tukki0210@gmail.com>
2023-07-20 14:54:18 +0900

    first commit

---
+++ othello.js
@@ -0,0 +1,41 @@
+const board = document.getElementById('board')
+
+let count = 0
+
+const game = []
+
+// よく使いそうな処理は関数にする
+function makeBlack(masu) {
+    masu.textContent = '●'
+    masu.classList.add('black')
+}
+
+function makeWhite(masu) {
+    masu.textContent = '●'
+    masu.classList.add('white')
+}
+
+// 最初は横一列（一次元）のオセロで考える
+for (let i = 0; i < 8; i++) {
+    const masu = document.createElement('div');
+
+    if (i === 27 || i === 36) {
+        makeBlack(masu)
+    } else if (i === 28 || i === 35) {
+        makeWhite(masu)
+    } else {
+        masu.addEventListener('click', () => {
+            if (count % 2 === 0) {
+                makeBlack(masu)
+            } else {
+                makeWhite(masu)
+            }
+            count++
+        }, {
+            once: true
+        })
+    }
+
+
+    board.append(masu);
+}
